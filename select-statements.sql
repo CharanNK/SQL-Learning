83c85998b82534bbab6656752cd6f18c303a3fc2
Charan NK <charkeet@gmail.com>
2021-07-06 00:10:21 +0530

    exploring select statements

---
+++ select-statements.sql
@@ -0,0 +1,6 @@
+--Select query runs row by row.
+
+
+SELECT 8 FROM EMPLOYEES; -- will display 8 in all the rows as the number of records in the table
+
+SELECT 'CHARAN' FROM EMPLOYEES; -- will display 8 in all the rows as the number of records in the table
